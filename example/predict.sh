da8d355da4b479c4c0830c71c502aabbad90823e
Loren Kuich <loren.jk3@gmail.com>
2019-03-21 03:40:57 +0000

    Worked on inference

---
+++ example/predict.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python predict --model 1553137884 --image rose.jpg
--- inference/predict.sh
+++
@@ -1,1 +0,0 @@
-python predict.oldest.py --graph graph.pb --image rose.jpg --labels opt/ml/model/1552280906/labels.csv
