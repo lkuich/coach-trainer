2c603cfb68e85a636f910dad1d6994e07fcf9630
Loren Kuich <lkuich@agentsofdiscovery.com>
2019-04-09 18:05:48 -0700

    Worked on serving

--- serve/predict.py
+++ serve/predict.py
@@ -24,6 +24,34 @@ def load_labels(label_file):
         label.append(l.rstrip())
     return label
 
+def read_tensor_from_base(base, input_height=224, input_width=224, input_mean=0, input_std=255):
+  input_name = "file_reader"
+  output_name = "normalized"
+
+  file_reader = tf.decode_base64(base, input_name)
+  '''
+  if file_name.endswith(".png"):
+    image_reader = tf.image.decode_png(
+        file_reader, channels=3, name="png_reader")
+  elif file_name.endswith(".gif"):
+    image_reader = tf.squeeze(
+        tf.image.decode_gif(file_reader, name="gif_reader"))
+  elif file_name.endswith(".bmp"):
+    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+  else:
+  '''
+  image_reader = tf.image.decode_jpeg(
+      file_reader, channels=3, name="jpeg_reader")
+
+  float_caster = tf.cast(image_reader, tf.float32)
+  dims_expander = tf.expand_dims(float_caster, 0)
+  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+  sess = tf.Session()
+  result = sess.run(normalized)
+
+  return result
+
 def read_tensor_from_image_file(file_name, input_height=224, input_width=224, input_mean=0, input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
