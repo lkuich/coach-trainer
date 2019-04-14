87118b9c07348db4ddd0a3b23d119cd768882f29
Loren Kuich <loren.jk3@gmail.com>
2019-04-14 05:32:05 +0000

    Fixes for predictions

--- serve/_predict.py
+++ serve/_predict.py
@@ -5,7 +5,7 @@ from tensorflow.keras.preprocessing import image
 import os
 import csv
 import numpy as np
-import json
+import argparse
 
 def load_graph(model_file):
   graph = tf.Graph()
@@ -24,34 +24,6 @@ def load_labels(label_file):
     for l in proto_as_ascii_lines:
         label.append(l.rstrip())
     return label
-
-def read_tensor_from_base(base, input_height=224, input_width=224, input_mean=0, input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-
-  file_reader = tf.decode_base64(base, input_name)
-  '''
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(
-        file_reader, channels=3, name="png_reader")
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(
-        tf.image.decode_gif(file_reader, name="gif_reader"))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-  else:
-  '''
-  image_reader = tf.image.decode_jpeg(
-      file_reader, channels=3, name="jpeg_reader")
-
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0)
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
-
-  return result
 
 def read_tensor_from_image_file(file_name, input_height=224, input_width=224, input_mean=0, input_std=255):
   input_name = "file_reader"
@@ -77,25 +49,20 @@ def read_tensor_from_image_file(file_name, input_height=224, input_width=224, in
 
   return result
 
-'''
 def get_input_args():
   parser = argparse.ArgumentParser()
   parser.add_argument("--image", help="image to be processed")
-  parser.add_argument("--model", help="model ID")
   return parser.parse_args()
-'''
 
-def predict(base):
-  '''
+def main():
   args = get_input_args()
   if (args.image is None):
-    print("Need exactly 2 arguments")
+    print("Need exactly 1 argument")
     return
-  '''
 
   prefix = '/opt/ml/model/'
 
-  graph = load_graph(os.path.join(prefix, 'frozen.pb'))
+  graph = load_graph(os.path.join(prefix, 'saved_model.pb'))
   labels = load_labels(os.path.join(prefix, 'labels.csv'))  
 
   output_name = "import/softmax_input/Softmax"
@@ -104,8 +71,7 @@ def predict(base):
   input_operation = graph.get_operation_by_name(input_name)
   output_operation = graph.get_operation_by_name(output_name)
 
-  #t = read_tensor_from_image_file('/home/loren/ScaledTrainer/serve/rose.jpg')
-  t = read_tensor_from_base(base)
+  t = read_tensor_from_image_file(args.image)
   
   with tf.Session(graph=graph) as sess:
     sess.run(tf.global_variables_initializer())
@@ -115,12 +81,9 @@ def predict(base):
   print(results)
   results = np.squeeze(results)
 
-  response = { "results": [] }
   top_k = results.argsort()[-5:][::-1]
   for i in top_k:
-    response['results'].append({
-      "label": labels[i],
-      "result": str(results[i])
-    })
     print(labels[i], results[i])
-  return json.dumps(response)
+
+if __name__ == "__main__":
+  main()
